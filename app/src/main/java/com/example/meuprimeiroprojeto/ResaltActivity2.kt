c7b2e48c8cc5b7f3279e9850215a5fd52c643026
rods1011 <taliane1011>
2023-03-04 01:31:16 -0300

    add result

---
+++ app/src/main/java/com/example/meuprimeiroprojeto/ResaltActivity2.kt
@@ -0,0 +1,51 @@
+package com.example.meuprimeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+
+class ResaltActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resalt2)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val tvResult = findViewById<TextView>(R.id.textView_result)
+        val tvClassificação = findViewById<TextView>(R.id.textview_classificação)
+
+        val result = intent.getFloatExtra("EXTRA_RESULT", 0.1f)
+
+        tvResult.text = result.toString()
+
+
+        val classificação = if (result < 18.5f) {
+            "ABAIXO DO PESO"
+        } else if (result in 18.5f..24.9f) {
+            "NORMAL"
+        } else if (result in 25f..29.9f) {
+            "SOBREPESO"
+        } else if (result in 30f..39.9f) {
+            "OBESIDADE"
+        } else {
+            "OBESIDADE GRAVE"
+        }
+
+        tvClassificação.text = getString(R.string.message_classificação,classificação)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
+
+
+
+
+
+
